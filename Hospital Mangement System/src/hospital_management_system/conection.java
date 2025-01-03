e0565681e132219e8d75486b81a5e0235ecac74f
SIAM0808 <fahadhasan1425@gmail.com>
2025-01-04 01:55:20 +0600

    updated files

---
+++ Hospital Mangement System/src/hospital_management_system/conection.java
@@ -0,0 +1,26 @@
+package hospital_management_system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class conection {
+    Connection connection;
+    Statement statement;
+
+    public conection() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital_management_system", "root", "565656");
+            if (connection != null) {
+                statement = connection.createStatement();
+                System.out.println("Database connected successfully!");
+            } else {
+                System.out.println("Failed to establish connection!");
+            }
+        } catch (Exception e) {
+            System.out.println("Error in database connection: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
+}
